dfee5df8c8f083935edc924add166e0e8a4b5129
Olga Kopaeva <olya.ant@gmail.com>
2020-12-18 15:14:01 +0300

    Modified homework Lesson7.

---
+++ HomeWorks/src/main/java/Lesson7/ApplicationGlobalState.java
@@ -0,0 +1,33 @@
+package Lesson7;
+
+public final class ApplicationGlobalState {
+
+    private static ApplicationGlobalState INSTANCE;
+    private String selectedCity = null;
+    private final String API_KEY = "PMEd71GhmmF0v26vIXdbuKee3EsGXGfB";
+
+    private ApplicationGlobalState() {
+    }
+
+    // Непотокобезопасный код для упрощения
+    public static ApplicationGlobalState getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new ApplicationGlobalState();
+        }
+
+        return INSTANCE;
+    }
+
+    public String getSelectedCity() {
+        return selectedCity;
+    }
+
+    public void setSelectedCity(String selectedCity) {
+        this.selectedCity = selectedCity;
+    }
+
+    public String getApiKey() {
+        return this.API_KEY;
+    }
+}
+
---
+++ HomeWorks/src/main/java/Lesson7/enums/Periods.java
@@ -0,0 +1,8 @@
+package Lesson7.enums;
+
+public enum Periods {
+    NOW,
+    FIVE_DAYS,
+    CUSTOM
+}
+
